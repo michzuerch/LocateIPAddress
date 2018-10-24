603cd774706f8f3d85b5f52af57918ed7b022c27
Michael Zuercher <michzuerch@gmail.com>
2018-10-24 18:26:28 +0200

    Remove the corrupt maven docker plugin, i am starting docker-compose...

--- src/main/java/com/gmail/michzuerch/locateipaddress/LocateIPAddressApplication.java
+++ src/main/java/com/gmail/michzuerch/locateipaddress/LocateIPAddressApplication.java
@@ -8,15 +8,6 @@ import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class LocateIPAddressApplication {
-
-    /**
-     * The main method makes it possible to run the application as a plain Java
-     * application which starts embedded web server via Spring Boot.
-     * <p>
-     * Ein Commit nach init
-     *
-     * @param args command line parameters
-     */
     public static void main(String[] args) {
         SpringApplication.run(LocateIPAddressApplication.class, args);
     }
