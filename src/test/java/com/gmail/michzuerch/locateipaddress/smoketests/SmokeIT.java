df0921e6d403176423ddddfff3559f87b5bfd42f
Michael Zürcher <michzuerch@gmail.com>
2018-11-17 23:47:07 +0100

    Moved Tests, Report mvn site

--- src/test/java/com/gmail/michzuerch/locateipaddress/smoketests/SmokeIT.java
+++ src/test/java/com/gmail/michzuerch/locateipaddress/smoketests/SmokeIT.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class SmokeTest {
+public class SmokeIT {
 
     @Autowired
     private SecurityTestRest controller;
